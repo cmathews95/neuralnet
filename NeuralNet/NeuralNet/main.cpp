271393d9b73fa867dc78e993e6827aff29b034b6
Christie Mathews <mathews.christie@gmail.com>
2015-10-05 19:16:49 -0700

    Created Xcode Project

---
+++ NeuralNet/NeuralNet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NeuralNet
+//
+//  Created by Christie Mathews on 10/5/15.
+//  Copyright (c) 2015 Christie Mathews. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
